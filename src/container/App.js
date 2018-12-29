6b7a5a254c8b69831be039e40718442ec7698d06
Arudovwen <successahon@gmail.com>
2018-12-29 22:22:51 +0100

    added fonts

--- src/container/App.js
+++ src/container/App.js
@@ -3,6 +3,7 @@ import CardList from '../component/CardList';
 import SearchBox from '../component/SearchBox';
 import Scroll from '../component/Scroll';
 import ErrorBoundary from '../container/ErrorBoundary';
+import './App.css';
 
 
 
